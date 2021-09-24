3ee545cfa72afd05e40b0d4319be6fd74b76bbea
Tasuku <cn.tasken@gmail.com>
2021-09-24 13:33:59 +0900

    build

--- js/client/index.js
+++ js/client/index.js
@@ -2,6 +2,7 @@ var target = document.querySelector("#target");
 aalib.read.video.fromVideoElement(target)
     .map(aalib.aa({width:530, height:160, colored:true}))
     .map(aalib.render.html({
+      tagName: 'div',
       fontSize: 7
     }))
     .subscribe();
